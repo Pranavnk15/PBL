d51760ee5ff4750cc41012d9ef2d0c3fc4f5e722
Pranav Khandagale <96475101+Pranavnk15@users.noreply.github.com>
2023-04-27 10:08:00 +0530

    Add files via upload
    Updated open api

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from keras.models import load_model
 import numpy as np
 import tensorflow as tf
 from PIL import Image, ImageOps
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
@@ -22,7 +23,37 @@ def import_n_pred(image_path, model):
     return dic[max_index]
 
 
+import openai
+openai.api_key = "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"
 
+def get_care_recommendations(plant_name):
+    prompt = f"What are the care recommendations in 200 words for {plant_name} plants? consider the points like watering, sunlight, season, and this plant is from india"
+    model = "text-davinci-002"
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    care_recommendations = response.choices[0].text.strip()
+    return care_recommendations
+
+@app.route('/submit', methods=['GET', 'POST'])
+def get_output():
+    if request.method == 'POST':
+        img = request.files['my_image']
+        img_path = 'static/' + img.filename
+        img.save(img_path)
+
+        predicted_plant = import_n_pred(img_path, model)
+        care_recommendations = get_care_recommendations(predicted_plant)
+        print(f"Predicted plant: {predicted_plant}")
+        print(f"Care recommendations: {care_recommendations}")
+
+    return render_template('model.html', prediction=predicted_plant, care_recommendations=care_recommendations, img_path=img_path)
 
 # def import_n_pred(img_path, model):
 #     img = Image.open(img_path)
@@ -42,18 +73,18 @@ def main():
 def page_model():
     return render_template('model.html')
 
-@app.route('/submit', methods=['GET', 'POST'])
-def get_output():
-    if request.method == 'POST':
-        img = request.files['my_image']
+# @app.route('/submit', methods=['GET', 'POST'])
+# def get_output():
+#     if request.method == 'POST':
+#         img = request.files['my_image']
 
-        img_path = 'static/' + img.filename
-        img.save(img_path)
+#         img_path = 'static/' + img.filename
+#         img.save(img_path)
 
-        p = import_n_pred(img_path, model)
-        print(p)
+#         p = import_n_pred(img_path, model)
+#         print(p)
 
-    return render_template('model.html', prediction=p, img_path=img_path)
+#     return render_template('model.html', prediction=p, img_path=img_path)
 
 
 
